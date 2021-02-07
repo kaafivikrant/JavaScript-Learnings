227ea58675aa0addf46192a1cee78e97ec6a0982
Vikrant Sharma <iamvikrant1@gmail.com>
2021-02-07 22:19:43 +0530

    hi

---
+++ API Testing/Vikrant/index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+console.log(`
+
+Hey,
+I am a final year student. I did a 9 months internship at Skoda Auto India as a software engineer and also solved 350+ leetcodes in the last 5 months along with multiple projects.
+I am proficient in C++, JavaScript and love Node Js.
+Currently having 8.2 CGPA.
+
+Email: iamvikrant1@gmail.com
+Github: https://github.com/kaafivikrant
+LinkedIn: https://linkedin.com/in/kaafivikrant
+Website: http://kaafivikrant.me/
+
+❤️
+
+`);
