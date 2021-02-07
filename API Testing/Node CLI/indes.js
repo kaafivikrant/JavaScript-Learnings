741b82578916731f3a5b22f010075d9fb8992553
Vikrant Sharma <iamvikrant1@gmail.com>
2021-02-07 14:57:29 +0530

    1.0.0

---
+++ API Testing/Node CLI/indes.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+console.log(`
+
+Sanya Kumari
+
+I am a final year student form Thapar University and currently doing my Internship at Amazon India. I have worked on C++ and web developement in my past internships.
+
+Website: http://sanya2508.github.io
+Email: 1701sanya@gmail.com
+Github: https://github.com/sanya2508
+LinkedIn: https://www.linkedin.com/in/sanyathisside/
+
+`);
