9088061130d9fdefc63c38c4614cb9bc4111fef3
kaafivikrant <iamvikrant1@gmail.com>
2021-11-04 11:57:55 +0530

    added

---
+++ CLI-Apps/File_Storage.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+// File Storage 100mb (104857600 bytes) from terminal
+
+var axios = require('axios');
+var FormData = require('form-data');
+var fs = require('fs');
+
+
+var form = new FormData();
+form.append('file', fs.createReadStream('/CLI-Apps/test.txt'), {filename: 'test.txt', contentType: 'image/jpeg'});
+
+
+// var date = new Date();
+// var dateString = date.getFullYear() + '-' + (date.getMonth()+1) + '-' + (date.getDate()+1) + 'T' + date.getHours() + '-' + date.getMinutes() + '-' + date.getSeconds();
+
+// form.append('expires', dateString)
+// form.append('maxDownloads',1);
+// form.append('autoDelete', true);
+
+// axios({
+//     method: 'post',
+//     url: 'https://file.io/',
+//     data: form,
+//     headers: { "Content-Type": "multipart/form-data" },
+// })
+// .then(function (response) {
+//    console.log("Congratulations!!"); 
+//    console.log(response.data.link);
+// })
+// .catch(function (error) {
+//     console.log(error);
+// });
---
+++ CLI-Apps/test_js.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+var file = fs.createReadStream(__dirname)
+
+console.log(file)
