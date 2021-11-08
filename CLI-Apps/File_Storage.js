e2ee6c8ffb8486bf96dc65dee0aeff952e7b064e
kaafivikrant <iamvikrant1@gmail.com>
2021-11-08 16:30:24 +0530

    hi

--- CLI-Apps/File_Storage.js
+++ CLI-Apps/File_Storage.js
@@ -5,28 +5,26 @@ var axios = require('axios');
 var FormData = require('form-data');
 var fs = require('fs');
 
-
 var form = new FormData();
-form.append('file', fs.createReadStream('/CLI-Apps/test.txt'), {filename: 'test.txt', contentType: 'image/jpeg'});
+form.append('file', fs.createReadStream('/CLI-Apps/newfile.txt'));
 
+var date = new Date();
+var dateString = date.getFullYear() + '-' + (date.getMonth()+1) + '-' + (date.getDate()+1) + 'T' + date.getHours() + '-' + date.getMinutes() + '-' + date.getSeconds();
 
-// var date = new Date();
-// var dateString = date.getFullYear() + '-' + (date.getMonth()+1) + '-' + (date.getDate()+1) + 'T' + date.getHours() + '-' + date.getMinutes() + '-' + date.getSeconds();
+form.append('expires', dateString)
+form.append('maxDownloads',1);
+form.append('autoDelete', true);
 
-// form.append('expires', dateString)
-// form.append('maxDownloads',1);
-// form.append('autoDelete', true);
-
-// axios({
-//     method: 'post',
-//     url: 'https://file.io/',
-//     data: form,
-//     headers: { "Content-Type": "multipart/form-data" },
-// })
-// .then(function (response) {
-//    console.log("Congratulations!!"); 
-//    console.log(response.data.link);
-// })
-// .catch(function (error) {
-//     console.log(error);
-// });
+axios({
+    method: 'post',
+    url: 'https://file.io/',
+    data: form,
+    headers: { "Content-Type": "multipart/form-data" },
+})
+.then(function (response) {
+   console.log("Congratulations!!"); 
+   console.log(response.data.link);
+})
+.catch(function (error) {
+    console.log(error);
+});
